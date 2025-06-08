093f199efa30da70fc6cb21e5a7d1ff8cd8bb5ce
NxNiki <michaelniki1988@gmail.com>
2025-06-08 07:16:35 +0000

    add hpo.py (wip)

--- udacity_mle/CD0387-deep-learning-topics-within-computer-vision-nlp-project-starter/hpo.py
+++ udacity_mle/CD0387-deep-learning-topics-within-computer-vision-nlp-project-starter/hpo.py
@@ -10,37 +10,125 @@ import torchvision.transforms as transforms
 
 import argparse
 
-def test(model, test_loader):
+def test(model, test_loader, criterion, hook=None):
     '''
     TODO: Complete this function that can take a model and a 
           testing data loader and will get the test accuray/loss of the model
           Remember to include any debugging/profiling hooks that you might need
     '''
-    pass
+    
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model=model.to(device)
+    
+    model.eval()
+    # ===================================================#
+    # 3. Set the SMDebug hook for the validation phase. #
+    # ===================================================#
+    if hook:
+        hook.set_mode(smd.modes.EVAL)
+        
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data=data.to(device)
+            target=target.to(device)
+            output = model(data)
+            # test_loss += F.nll_loss(output, target, reduction="sum").item()  # sum up batch loss
+            test_loss += criterion(output, target)
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
-def train(model, train_loader, criterion, optimizer):
+    test_loss /= len(test_loader.dataset)
+
+    print(
+        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
+        )
+    )
+    
+
+def train(model, train_loader, epochs, criterion, optimizer, hook=None):
     '''
     TODO: Complete this function that can take a model and
           data loaders for training and will get train the model
           Remember to include any debugging/profiling hooks that you might need
     '''
-    pass
+    
+    model.train()
+
+    if hook:
+        hook.set_mode(smd.modes.TRAIN)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model=model.to(device)
+
+    for epoch in range(epochs):
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data=data.to(device)
+            target=target.to(device)
+            optimizer.zero_grad()
+            output = model(data)
+            # loss = F.nll_loss(output, target)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+            if batch_idx % 100 == 0:
+                print(
+                    "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                        epoch,
+                        batch_idx * len(data),
+                        len(train_loader.dataset),
+                        100.0 * batch_idx / len(train_loader),
+                        loss.item(),
+                    )
+                )
     
 def net():
     '''
     TODO: Complete this function that initializes your model
           Remember to use a pretrained model
     '''
-    pass
 
-def create_data_loaders(data, batch_size):
+    model = models.resnet18(weights='DEFAULT')
+    for param in model.parameters():
+        param.requires_grad = False   
+
+    num_features=model.fc.in_features
+    model.fc = nn.Sequential(
+                   nn.Linear(num_features, 133))
+    
+    return model
+
+
+def create_data_loaders(data_train, data_valid, data_test, batch_size):
     '''
     This is an optional function that you may or may not need to implement
     depending on whether you need to use data loaders or not
     '''
-    pass
+
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),  # Resize to 224x224
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], 
+                             [0.229, 0.224, 0.225])  # ImageNet normalization
+    ])
+
+    train_dataset = datasets.ImageFolder(root=data_train, transform=transform)
+    valid_dataset = datasets.ImageFolder(root=data_valid, transform=transform)
+    test_dataset  = datasets.ImageFolder(root=data_test,  transform=transform)
+    
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+    test_loader  = DataLoader(test_dataset,  batch_size=batch_size, shuffle=False, num_workers=2)
+
+    return train_loader, valid_loader, test_loader
 
 def main(args):
+
+    train_loader, valid_loader, test_loader = create_data_loaders(
+        args.data_train, args.data_valid, args.data_test, args.batch_size
+    )
     '''
     TODO: Initialize a model by calling the net function
     '''
@@ -49,8 +137,8 @@ def main(args):
     '''
     TODO: Create your loss and optimizer
     '''
-    loss_criterion = None
-    optimizer = None
+    loss_criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.fc.parameters(), lr=0.001)
     
     '''
     TODO: Call the train function to start training your model
@@ -67,6 +155,7 @@ def main(args):
     TODO: Save the trained model
     '''
     torch.save(model, path)
+
 
 if __name__=='__main__':
     parser=argparse.ArgumentParser()
